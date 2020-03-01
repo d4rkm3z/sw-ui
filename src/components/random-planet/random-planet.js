34b0f7753ef8d7f455f50cd01719cc2e069da7f9
Kirill Sadovnikov <d4rkm3z@gmail.com>
2020-03-01 15:14:25 +0300

    feat: implement transform data and data loading at random-planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,34 +1,59 @@
-import React, { Component } from 'react';
+import React, {useCallback, useLayoutEffect, useState} from 'react';
+import SwapiService from '../../services/SwapiService';
 
 import './random-planet.css';
 
-export default class RandomPlanet extends Component {
+const service = new SwapiService();
 
-
-  render() {
-    return (
-      <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-             src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
-        <div>
-          <h4>Planet Name</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>123124</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>43</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>100</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-    );
-  }
+function getRandomId() {
+    return Math.floor(Math.random() * 19) + 2;
 }
+
+export default React.memo(function RandomPlanet() {
+    const [id, setId] = useState(null);
+    const [name, setName] = useState(null);
+    const [population, setPopulation] = useState(null);
+    const [rotationPeriod, setRotationPeriod] = useState(null);
+    const [diameter, setDiameter] = useState(null);
+
+    const onPlanetLoaded = useCallback(({
+                                            name,
+                                            population,
+                                            diameter,
+                                            rotationPeriod
+                                        }) => {
+        setId(getRandomId());
+        setName(name);
+        setPopulation(population);
+        setRotationPeriod(rotationPeriod);
+        setDiameter(diameter);
+    }, []);
+
+    useLayoutEffect(() => {
+        service.getPlanet(getRandomId()).then(onPlanetLoaded);
+    }, [onPlanetLoaded]);
+
+    return (
+        <div className="random-planet jumbotron rounded">
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+            <div>
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+})
--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -1,5 +1,5 @@
 export default class SwapiService {
-    apiBase = 'https://swapi.co/api/';
+    apiBase = 'https://swapi.co/api';
 
     async getResource(url) {
         const res = await fetch(`${this.apiBase}${url}`);
@@ -12,23 +12,69 @@ export default class SwapiService {
 
     async getAllPeople() {
         const res = await this.getResource(`/people/`);
-        return res.results;
+        return res.results.map(this._transformPerson);
     }
 
     getPerson(id) {
-        return this.getResource(`/people/${id}`);
+        const res = this.getResource(`/people/${id}/`);
+        return this._transformPerson(res);
     }
 
-    getPlanet(id) {
-        return this.getResource('/planets/');
+    async getAllPlanets() {
+        const res = await this.getResource('/planets/');
+        return res.results.map(this._transformPlanetData);
+    }
+
+    async getPlanet(id) {
+        const planet = await this.getResource(`/planets/${id}/`);
+        return this._transformPlanetData(planet);
     }
 
     async getAllStarships() {
         const res = await this.getResource('/starships/');
-        return res.results;
+        return res.results.map(this._transformStarship);
     }
 
     getStarship(id) {
-        return this.getResource(`/starships/${id}`);
+        const res = this.getResource(`/starships/${id}/`);
+        return this._transformStarship(res);
+    }
+
+    _extractId(item) {
+        const idRegExp = /\/(\d+)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    _transformStarship(starship) {
+        return {
+            id: this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.costInCredits,
+            length: starship.length,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargoCapacity
+        }
+    }
+
+    _transformPerson(person) {
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birthYear,
+            eyeColor: person.eyeColor
+        }
+    }
+
+    _transformPlanetData(planet) {
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+        }
     }
 }
